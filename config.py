accb58a3c121aceea8029961dc3b4ed0bacb117b
my#h <myth@localhost.localdomain>
2017-02-23 15:33:01 +0530

    slack api token is added in config

---
+++ config.py
@@ -0,0 +1,1 @@
+stock_analyst_slack_bot="slack api key"
--- slackbot.py
+++ slackbot.py
@@ -1,9 +1,8 @@
 import requests
+import config
 import urllib
 
-
-
 def buy_call(data):
-    final_url ='https://slack.com/api/chat.postMessage?token=xoxp-144602751008-145388246788-145241612643-bea8b2229957eb06bfc3a89ead7686ca&channel=%23buy_call&text='+urllib.quote(data)+'&username=stock_analyst&xoxp-144602751008-145388246788-145241612643-bea8b2229957eb06bfc3a89ead7686ca&pretty=1'
+    final_url ='https://slack.com/api/chat.postMessage?token='+config.stock_analyst_slack_bot+'&channel=%23buy_call&text='+urllib.quote(data)+'&username=stock_analyst&'+config.stock_analyst_slack_bot+'&pretty=1'
     response = requests.get(final_url)
     print(response)
